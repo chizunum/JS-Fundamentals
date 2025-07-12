06f03cc4300839a22ad6cfda05485ff039151ff9
Chizunum Nlerum <Chizunum@gmail.com>
2025-07-12 05:36:39 +0100

    Checking for 3 argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,8 @@
 
 const args = process.argv.slice(2);
 
-if (args.length === 0) {
-    console.log("No argument");
+if (args[0] === undefined) {
+  console.log("No argument");
 } else {
-    console.log(args[0]);
+  console.log(args[0]);
 }
